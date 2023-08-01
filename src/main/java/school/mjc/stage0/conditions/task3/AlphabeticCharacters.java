921261b03b8fdddd5981c12ea8d1d65ff2c0df92
Iurii Cherkasov <cherkasov1010@gmail.com>
2023-08-01 18:57:32 +0300

    m4 t3

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if ((int) character >= 97 && (int) character <= 122 || (int) character >= 65 && (int) character <= 90) {
+            if (character == 'a' || character == 'o' || character == 'A' || character == 'O' || character == 'e'
+                    || character == 'E' || character == 'i' || character == 'I' || character == 'u'
+                    || character == 'U' || character == 'y' || character == 'Y') {
+                System.out.println("Vowel");
+            } else {
+                System.out.println("Consonant");
+            }
+        } else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -1,6 +1,22 @@
 package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
+    //Basic Salary <= 10000: taxes - 15%.
+    // 10000 < Basic Salary <= 20000: taxes - 18% Basic Salary > 20000 : taxes - 20% Basic salary < 0 -> "wrong input!"
     public void calculateSalary(int salary) {
+        double input = 0.0;
+        if (salary < 0) {
+            System.out.println("wrong input!");
+            return;
+        }
+        if (salary <= 10000) {
+            input = (double) salary / 100.0 * 15;
+        } else if (salary > 10000 && salary <= 20000) {
+            input = (double) salary / 100.0 * 18;
+        } else if (salary > 20000) {
+            input = (double) salary / 100.0 * 20;
+        }
+        input = (double) salary - input;
+        System.out.println(input);
     }
 }
